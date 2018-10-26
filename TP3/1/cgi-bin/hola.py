317d4b929b9fbb922d6658def9f633a6835a7be4
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2018-10-26 14:14:49 -0300

    corrigiendo entorno

--- TP3/1/cgi-bin/hola.py
+++ TP3/1/cgi-bin/hola.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 print("Content-type: text/html\n")
 print()
-print("<HTML><BODY>\n")
+print("<HTML><HEAD><title>Algo</title></HEAD><BODY>\n")
 print("<font color = blue>\n")
 print("<TITLE>CGI script output</TITLE>")
 print("<h1 style=\"TEXT-ALIGN: center\">HOLA MUNDO</h1>\n")
 print("</font>\n")
 print("</HTML>")
- 
